e4f5e948a2e2f9a07887f53a9e06bed9e5983018
Adam, Kelli <AdamAndrus11@gmail.com>
2016-08-10 14:57:23 -0700

    initial commit of story 1

---
+++ battleship.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("")
+  //purpose: Creating a 10 x 10 grid using for loops to append columns to a row.
+  //signature: take html elements of <tr> and <td> and loops to 10 to ultimately create a 10 x 10 grid.
+  //example: function gameGrid() ----> 10 rows and columns in html file.
+  function gameGrid(){
+    //a function called gameGrid()
+    for(var trIndex = 0; trIndex < 10; trIndex++){
+      //for loop that makes 10 <tr>
+      $("table").append("<tr></tr>");
+      //appending <tr>'s
+        for(var tdIndex = 0; tdIndex < 10; tdIndex++) {
+          //for loop that makes 10 <td>
+          // <td id="fakeID"></td>
+          // "1" + 2 + 3
+          $("tr").last().append('<td id="' + trIndex + tdIndex + '"></td>');
+          //
+          $("td").addClass("field") ;
+        }
+    }
+  };
+  //calling gameGrid();
+  gameGrid();
+});//end of doc ready function
